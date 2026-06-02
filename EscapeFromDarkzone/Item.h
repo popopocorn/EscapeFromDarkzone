9d666f03e0cbe8cd76c21fa41894557314594100
popopocorn <popcorn375@naver.com>
2026-06-02 23:01:10 +0900

    중복선언 수정

--- EscapeFromDarkzone/Item.h
+++ EscapeFromDarkzone/Item.h
@@ -41,23 +41,6 @@ struct WeaponSpec
 	float reloadTime = 0.0f;
 };
 
-////재료를 위한 구조
-//enum class ItemID {
-//	//재료
-//	NONE = 0,
-//	MAT_1_FIBER,
-//	MAT_2_METAL_PLATE,
-//	MAT_3_BOLT_AND_NUT,
-//
-//	WEAPON_UPGRADE_1,
-//	WEAPON_UPGRADE_2,
-//	WEAPON_UPGRADE_3,
-//	WEAPON_UPGRADE_4,
-//	ARMOR_PLATE,
-//
-//	//완성품
-//
-//};
 
 struct RecipeElement {
 	ItemID itemID;
