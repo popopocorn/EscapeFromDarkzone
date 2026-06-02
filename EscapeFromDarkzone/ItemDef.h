009545388ea23c802f486e2c6e78fddbcc1485df
popopocorn <popcorn375@naver.com>
2026-06-02 22:52:16 +0900

    중복선언 수정

--- EscapeFromDarkzone/ItemDef.h
+++ EscapeFromDarkzone/ItemDef.h
@@ -1,12 +1,23 @@
 #pragma once
 
-enum class ItemID : short {
-    NONE = 0,
-    MAT_1, MAT_2, MAT_3, MAT_4, MAT_5, MAT_6,
-    WEAPON_UPGRADE_1, WEAPON_UPGRADE_2, WEAPON_UPGRADE_3, WEAPON_UPGRADE_4,
-    ARMOR_PLATE,
-};
 
+//재료를 위한 구조
+enum class ItemID {
+	//재료
+	NONE = 0,
+	MAT_1_FIBER,
+	MAT_2_METAL_PLATE,
+	MAT_3_BOLT_AND_NUT,
+
+	WEAPON_UPGRADE_1,
+	WEAPON_UPGRADE_2,
+	WEAPON_UPGRADE_3,
+	WEAPON_UPGRADE_4,
+	ARMOR_PLATE,
+
+	//완성품
+
+};
 struct ItemSlot {
     ItemID item = ItemID::NONE;
     int    count = 0;
