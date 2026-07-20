2f6e15361aac633009bd0fb72f1b8a3d93b277a7
popopocorn <popcorn375@naver.com>
2026-07-20 15:11:47 +0900

    Update Scene.h

--- EscapeFromDarkzone/Scene.h
+++ EscapeFromDarkzone/Scene.h
@@ -88,7 +88,7 @@ public:
 
 	virtual void BuildDefaultLightsAndMaterials();
 	virtual void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList) {};
-	void ReleaseObjects() {};
+	virtual void ReleaseObjects() {};
 
 	void SetRoot(ID3D12RootSignature* root) { m_pd3dGraphicsRootSignature = root; }
 
